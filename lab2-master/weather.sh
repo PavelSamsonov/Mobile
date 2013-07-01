90f86e0687ec28bb84b336bfca287af4ecc6b4de
PavelSamsonov <samspavel2@gmail.com>
2013-07-01 10:02:36 +0300

    Update weather.sh
    Updated func

--- lab2-master/weather.sh
+++ lab2-master/weather.sh
@@ -16,7 +16,7 @@ then
 	while true  
 	do  	
 		wget -q -O- $url | iconv -f cp1251 -t utf8>tmp.txt>> tmp.txt
-		weather=`cat ./tmp.txt | grep -A 1 "погода фактическая"| sed 's/<br>/\n/g'|sed 's/<[^>]*>/ /g'`
+		weather=`cat ./tmp.txt | grep -A 1 "погодa фактическая"| sed 's/<br>/\n/g'|sed 's/<[^>]*>/ /g'`
 		echo "$weather"
 		echo
 		echo
